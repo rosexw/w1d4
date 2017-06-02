f571d70f943b043c83014058191bb9ff4e9a1ae7
Rose Wan <rwan@mail.com>
2017-06-02 03:14:17 +0000

    example for closures to test. Node doesn't work properly for these

---
+++ closuresinstr.js
@@ -0,0 +1,44 @@
+/*Closures example*/
+var outer = function() {
+  var x = 10;
+
+  var inner = function() {
+    console.log("The value of x is: " + x);
+  }
+  return inner;
+};
+
+var foo = outer();
+foo();
+
+/*
+What will foo() print? Will x be 10, undefined or something else? What would make sense to you?
+
+Try it in the REPL. You'll discover that it prints The value of x is: 10. Why? Because the above code is executed in the following manner.
+
+Function outer is invoked: outer().
+outer assigns the value 10 to the variable x.
+outer defines a function and assigns it to the variable inner.
+outer returns the value of inner – the just-defined function.
+inner function is assigned to the variable foo.
+Function foo is invoked, logging The value of x is: 10 to the console.
+The key attribute of closures is demonstrated here: the inner function accesses variable x, which is defined within the parent function outer.
+*/
+
+/*
+Immediately-invoked function expression
+You can alternatively use an immediately-invoked function expression (IIFE) to achieve a similar result.
+*/
+
+var foo = (function() {
+  var x = 10;
+
+  var inner = function() {
+    console.log("Value of x is: " + x);
+  }
+  return inner;
+})(); // Notice the () at the end,
+      // which will immediately invoke the function,
+      // assigning the returned value (inner) to foo
+
+foo();
